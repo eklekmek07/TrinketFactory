c3e9f09c7f9a02fa202d2e7c3c7b6569e3a8daab
Barış <baris_kilinc@windowslive.com>
2023-05-25 14:17:34 +0300

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TrinketFactoryStore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
