d018ba39237ee2feffdb5b569c0a576ffe6b69d8
Barış <baris_kilinc@windowslive.com>
2023-06-06 22:22:16 +0300

    ya hak

---
+++ TrinketFactoryWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TrinketFactoryWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
